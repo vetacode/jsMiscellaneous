8e20089964dbb91eaac50622f89319675205f333
vetacode <vetacode@gmail.com>
2025-12-02 09:28:45 +0700

    task 2 done

--- proxyReflect.js
+++ proxyReflect.js
@@ -141,3 +141,64 @@ array = new Proxy(array, {
 console.log(array[1]); // 2
 console.log(array[-1]); // 3
 console.log(array[-2]); // 2
+
+/**TASK 3
+ * Observable
+Create a function makeObservable(target) that “makes the object observable” by returning a proxy.
+
+Here’s how it should work:
+
+function makeObservable(target) {
+  /* your code /
+}
+
+let user = {};
+user = makeObservable(user);
+
+user.observe((key, value) => {
+  alert(`SET ${key}=${value}`);
+});
+
+user.name = "John"; // alerts: SET name=John
+In other words, an object returned by makeObservable is just like the original one, but also has the method observe(handler) that sets handler function to be called on any property change.
+
+Whenever a property changes, handler(key, value) is called with the name and value of the property.
+
+P.S. In this task, please only take care about writing to a property. Other operations can be implemented in a similar way.
+ */
+{
+  let handlers = Symbol('handlers');
+
+  function makeObservable(target) {
+    // 1. Initialize handlers store
+    target[handlers] = [];
+
+    // Store the handler function in array for future calls
+    target.observe = function (handler) {
+      this[handlers].push(handler);
+    };
+
+    // 2. Create a proxy to handle changes
+    return new Proxy(target, {
+      set(target, property, value, receiver) {
+        let success = Reflect.set(...arguments); // forward the operation to object
+        if (success) {
+          // if there were no error while setting the property
+          // call all handlers
+          target[handlers].forEach((handler) => handler(property, value));
+        }
+        return success;
+      },
+    });
+  }
+
+  let user = {};
+
+  user = makeObservable(user);
+
+  user.observe((key, value) => {
+    console.log(`SET ${key}=${value}`); //'SET name=John'
+  });
+
+  user.name = 'John';
+}
