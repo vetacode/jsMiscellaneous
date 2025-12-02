60db867b8f790c3e77fcb9be566ad167499f7fed
vetacode <vetacode@gmail.com>
2025-12-02 08:55:41 +0700

    addomg proxy handler methods

--- proxyReflect.js
+++ proxyReflect.js
@@ -1,16 +1,37 @@
 'use strict';
 
-//PROXY
+//PROXY: is a special “exotic object”. It doesn’t have own properties
 //Syntax: let proxy = new Proxy(target, handler)
 //iterable
 
 let target = {};
 let proxy = new Proxy(target, {});
 
-proxy.test = 10;
-console.log(proxy.test);
-console.log(target.test);
+proxy.test = 10; //set value on the target
+console.log(proxy.test); //return value from target
+console.log(target.test); //return value from target
 
 for (let key in proxy) {
-  console.log(key); //test
+  console.log(key); //test, return value from target
 }
+
+//PROXY HANDLER METHOD
+
+/**
+ * 
+Internal Method       |	Handler Method            |	Triggers when…
+------------------------------------------------------------------------------------------------------------
+[[Get]]	              | get	                     |  reading a property
+[[Set]]               |	set                      |	writing to a property
+[[HasProperty]]	      | has                      |  in operator
+[[Delete]]            |	deleteProperty           |	delete operator
+[[Call]]              |	apply                    |	function call
+[[Construct]]         |	construct                |	new operator
+[[GetPrototypeOf]]    |	getPrototypeOf	         |  Object.getPrototypeOf
+[[SetPrototypeOf]]    |	setPrototypeOf           |	Object.setPrototypeOf
+[[IsExtensible]]      |	isExtensible             |	Object.isExtensible
+[[PreventExtensions]] |	preventExtensions        |	Object.preventExtensions
+[[DefineOwnProperty]] |	defineProperty           |	Object.defineProperty, Object.defineProperties
+[[GetOwnProperty]]    |	getOwnPropertyDescriptor |	Object.getOwnPropertyDescriptor, for..in, Object.keys/values/entries
+[[OwnPropertyKeys]]   |	ownKeys	                 |  Object.getOwnPropertyNames, Object.getOwnPropertySymbols, for..in, Object.keys/values/entries
+ */
